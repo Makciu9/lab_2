b3e44fdbcd89767c2af3fe103a3d19097fd272f0
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:51:48 +0300

    Scripted auto-commit on change (2020-03-13 20:51:48) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -1,11 +1,14 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.Ma;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class FliMapper extends Mapper<> {
     @Override
-    protected void map(LongWritable key, Text value, Context context){
+    protected void map(LongWritable key, Text value, Mapper.Context context){
 
+        String[] line = value.toString().split(",");
+        
     }
 }
 
