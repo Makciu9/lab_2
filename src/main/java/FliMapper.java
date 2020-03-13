9bcd2a3c2d8ec0bf60723f5032db076bca5fdc34
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 21:05:02 +0300

    Scripted auto-commit on change (2020-03-13 21:05:02) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -2,13 +2,18 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import static java.lang.Integer.parseInt;
+
 public class FliMapper extends Mapper<> {
     private static final int CANCELLED=19;
-    
+
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context){
 
         String[] line = value.toString().split(",");
+        if(parseInt(line[CANCELLED]) == 0){
+            
+        }
 
     }
 }
