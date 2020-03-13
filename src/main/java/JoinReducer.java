b7e0c8b33b04ccc089d60589a8120616e1be7824
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 00:22:23 +0300

    Scripted auto-commit on change (2020-03-14 00:22:23) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -1,6 +1,12 @@
-public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<AirWritable, Text, String, String> {
     @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+    protected void reduce(AirWritable key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text systemInfo = new Text(iter.next());
