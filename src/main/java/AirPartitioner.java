b33cc7dd3fc06d67a2f30c20e2535b8e5d4843d5
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:23:39 +0300

    Scripted auto-commit on change (2020-03-13 20:23:39) by gitwatch.sh

--- src/main/java/AirPartitioner.java
+++ src/main/java/AirPartitioner.java
@@ -1,3 +1,9 @@
-public class AirPartitioner {
-    
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirPartitioner extends Partitioner {
+
+    @Override
+    public int getPartition(Object o, Object o2, int i) {
+        return 0;
+    }
 }
