87d0ca81a0a02896d5eb456272a176166b1ae48a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 01:39:26 +0300

    Scripted auto-commit on change (2020-03-14 01:39:26) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -2,11 +2,12 @@ import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.jobcontrol.Job;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.Mapper;
 
 import javax.xml.soap.Text;
 import java.nio.file.Path;
 
-public class AirportApp {
+public class AirportApp extends Mapper<> {
     public static void main(String[] args) {
         if (args.length != 3) {
             System.err.println("Usage: AirportApp <input Air> <input Fli> <output path>");
