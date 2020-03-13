14cc85a9ef079ac676dc05909a6a88152891ab90
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:31:01 +0300

    Scripted auto-commit on change (2020-03-13 20:31:01) by gitwatch.sh

--- src/main/java/AirPartitioner.java
+++ src/main/java/AirPartitioner.java
@@ -4,13 +4,5 @@ import javax.xml.soap.Text;
 
 public class AirPartitioner extends Partitioner<AirWritable> {
 
-    @Override
-    public int getPartition(AirWritable key, int i) {
-        return key.getCode() % i;
-    }
 
-    @Override
-    public int getPartition(Object o, Object o2, int i) {
-        return 0;
-    }
 }
