d81ea0d84ebe5852b439398a27c33deb9d26a686
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 21:53:58 +0300

    Scripted auto-commit on change (2020-03-13 21:53:58) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -2,8 +2,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.IOException;
-
 import static java.lang.Integer.parseInt;
 
 public class FliMapper extends Mapper<> {
@@ -12,7 +10,7 @@ public class FliMapper extends Mapper<> {
     private static final int ARR_DELAY_NEW = 18;
 
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context) {
 
         String[] line = value.toString().split(",");
         if (parseInt(line[CANCELLED]) == 0) {
