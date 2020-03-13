d92ad6be226afb6733aa188d500f7f8ec8837e4d
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 02:02:56 +0300

    Scripted auto-commit on change (2020-03-14 02:02:56) by gitwatch.sh

--- src/main/java/AirMapper.java
+++ src/main/java/AirMapper.java
@@ -15,7 +15,7 @@ public class AirMapper extends Mapper<LongWritable, Text, AirWritable, Text> {
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
             String[] line = value.toString().replace("\"","").split(",(!?)");
-
+                    if(!line[0]=="")
                     AirWritable m_write = new AirWritable();
                     m_write.setFlag(0);
                     m_write.setCode(parseInt(line[CODE]));
