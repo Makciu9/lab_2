7b3229bfb59cd534f00eaf7f90c75952faacfa14
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 01:07:16 +0300

    Scripted auto-commit on change (2020-03-14 01:07:16) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -10,7 +10,7 @@ public class JoinReducer extends Reducer<AirWritable, Text, String, String> {
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         String AirportName = iter.next().toString();
-        String info = "{
+        String info = " =====>>{ ";
         if(iter.hasNext()){
         int counter = 0;
         Double min = Double.parseDouble(iter.next().toString());
@@ -30,6 +30,6 @@ public class JoinReducer extends Reducer<AirWritable, Text, String, String> {
             sum += currVal;
             counter++;
         }
-
+            info =
     }
 }
