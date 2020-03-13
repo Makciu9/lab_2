1c20ff5ecd3c9300d190fee24d66ddb971577864
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 02:26:44 +0300

    Scripted auto-commit on change (2020-03-14 02:26:43) by gitwatch.sh

--- src/main/java/GroupingComparator.java
+++ src/main/java/GroupingComparator.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 
-public class GroupingComparator  {
+public class GroupingComparator extends WritableComparator {
     protected GroupingComparator(){
      super(AirWritable.class, true);
     }
