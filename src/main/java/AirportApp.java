f1b4e274b985308efed46d0ba0b870310cc557fa
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 12:53:08 +0300

    Scripted auto-commit on change (2020-03-13 12:53:08) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -1,2 +1,27 @@
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.jobcontrol.Job;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+
+import java.nio.file.Path;
+
 public class AirportApp {
+    public static void main(String[] args) {
+
+
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
