3ca8d04c482bddffc11fe30c12d3e3b73ef0f51c
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 13:32:30 +0300

    Scripted auto-commit on change (2020-03-13 13:32:30) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -1,2 +1,17 @@
-public class FliMapper {
+public class FliMapper extends Mapper<> {
+}
+
+
+
+● public class WordMapper extends Mapper<> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws
+            IOException, InterruptedException {
+        String line = value.toString();
+        String[] words =
+                StringTools.split(StringTools.removeAllNonSymbols(line).toLowerCase());
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
 }
