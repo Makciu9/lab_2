c01f5c0d74a9b807414b85d8c52ea85c4cd705c7
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:43:35 +0300

    Scripted auto-commit on change (2020-03-13 20:43:35) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -1,7 +1,9 @@
+import org.apache.hadoop.io.LongWritable;
+
 public class FliMapper extends Mapper<> {
     @Override
-    protected void map(){
-    
+    protected void map(LongWritable key, Text value, Context context){
+
     }
 }
 
