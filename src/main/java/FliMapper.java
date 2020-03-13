5e8205d260f368b251247907464f9f0848cbd1c9
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 21:10:18 +0300

    Scripted auto-commit on change (2020-03-13 21:10:18) by gitwatch.sh

--- src/main/java/FliMapper.java
+++ src/main/java/FliMapper.java
@@ -6,13 +6,14 @@ import static java.lang.Integer.parseInt;
 
 public class FliMapper extends Mapper<> {
     private static final int CANCELLED=19;
+    private static final int DEST_AEROPORT_ID=14;
 
     @Override
     protected void map(LongWritable key, Text value, Mapper.Context context){
 
         String[] line = value.toString().split(",");
         if(parseInt(line[CANCELLED]) == 0){
-            if(!)
+            if(!DEST_AEROPORT_ID)
         }
 
     }
