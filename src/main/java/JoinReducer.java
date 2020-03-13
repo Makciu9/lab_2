206e799b4cdaec579d1fea14456c5f6525915b1d
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-14 01:16:17 +0300

    Scripted auto-commit on change (2020-03-14 01:16:17) by gitwatch.sh

--- src/main/java/JoinReducer.java
+++ src/main/java/JoinReducer.java
@@ -11,31 +11,32 @@ public class JoinReducer extends Reducer<AirWritable, Text, String, String> {
         Iterator<Text> iter = values.iterator();
         String AirportName = iter.next().toString();
         String info = null;
-        if(iter.hasNext()){
-        int counter = 0;
-        Double min = Double.parseDouble(iter.next().toString());
-        Double max = Double.MIN_VALUE;
-        Double currVal = null;
-        Double sum = null;
-        while (iter.hasNext()) {
-            currVal =Double.parseDouble(iter.next().toString());
+        if (iter.hasNext()) {
+            int counter = 0;
+            Double min = Double.parseDouble(iter.next().toString());
+            Double max = Double.MIN_VALUE;
+            Double currVal = null;
+            Double sum = null;
+            while (iter.hasNext()) {
+                currVal = Double.parseDouble(iter.next().toString());
 
-            if (currVal > max){
-                max = currVal;
-            }
+                if (currVal > max) {
+                    max = currVal;
+                }
 
-            if (currVal < min){
-                min = currVal;
+                if (currVal < min) {
+                    min = currVal;
+                }
+                sum += currVal;
+                counter++;
             }
-            sum += currVal;
-            counter++;
-        }
             info += " =====>>{ " +
                     "Min: " + min + "|" +
                     "Max: " + max + "|" +
-                    "AVG: " + sum/counter + "|" +
+                    "AVG: " + sum / counter + "|" +
                     "}  ";
-        
+            context.write(AirportName, info);
 
+        }
     }
 }
