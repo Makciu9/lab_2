a480261a821effe47b1d08ff3eceef11040ed9e6
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 19:19:51 +0300

    Scripted auto-commit on change (2020-03-13 19:19:50) by gitwatch.sh

--- src/main/java/AirportApp.java
+++ src/main/java/AirportApp.java
@@ -24,9 +24,11 @@ public class AirportApp {
         job.setPartitionerClass(AirPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
+        
         job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
