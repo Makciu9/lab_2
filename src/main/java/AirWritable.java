33ec93ce4b4aadc1c9328d4759119178b79aa5e3
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:00:40 +0300

    Scripted auto-commit on change (2020-03-13 20:00:40) by gitwatch.sh

--- src/main/java/AirWritable.java
+++ src/main/java/AirWritable.java
@@ -4,5 +4,21 @@ public class AirWritable implements WritableComparable {
     private int flag;
     private int code;
 
-    
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+
 }
