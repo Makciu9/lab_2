e9538afa305d7a599f289befa68bd972458a9c4e
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 21:42:43 +0300

    Scripted auto-commit on change (2020-03-13 21:42:43) by gitwatch.sh

--- src/main/java/AirpotrMapper.java
+++ src/main/java/AirpotrMapper.java
@@ -8,8 +8,8 @@ import static java.lang.Integer.parseInt;
 
 public class AirpotrMapper {
 
-        private static final int CODE = 19;
-        private static final int DEST_AEROPORT_ID = 14;
+        private static final int CODE = 0;
+        private static final int DESCRIPTION = 1;
 
         @Override
         protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
@@ -17,7 +17,7 @@ public class AirpotrMapper {
             String[] line = value.toString().split(",");
 
                     AirWritable m_write = new AirWritable();
-                    m_write.setFlag(1);
+                    m_write.setFlag(0);
                     m_write.setCode(parseInt(line[DEST_AEROPORT_ID]));
                     Text time = new Text(line[ARR_DELAY_NEW]);
                     context.write(m_write, time);
