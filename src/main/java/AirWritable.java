b3a4a642e724ea26151661f671b1fffc511481d3
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-13 20:02:09 +0300

    Scripted auto-commit on change (2020-03-13 20:02:09) by gitwatch.sh

--- src/main/java/AirWritable.java
+++ src/main/java/AirWritable.java
@@ -1,5 +1,9 @@
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirWritable implements WritableComparable {
     private int flag;
     private int code;
@@ -20,5 +24,18 @@ public class AirWritable implements WritableComparable {
         return flag;
     }
 
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
